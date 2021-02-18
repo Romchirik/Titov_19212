fb94abb9034c7ea972168b0ced4310a9144a8ca7
Romchirik <r.titov@g.nsu.ru>
2021-02-18 12:35:46 +0700

    Начал переносить проект на Maven

--- Befunge/src/ru/nsu/Instruction.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu;
-
-import java.util.Deque;
-
-import ru.nsu.InstructionPointer;
-
-public interface Instruction {
-    boolean exec(Deque<Integer> context, InstructionPointer instructionPointer);
-}
